34b23bc9ec17ef23bd9f45cc9c64e60fee4c48b4
AaG7xNnrgbzeyqc5woPS <47978362+AaG7xNnrgbzeyqc5woPS@users.noreply.github.com>
2020-01-10 16:14:24 +0800

    Create umount_whitenode_img.sh

---
+++ tools/umount_whitenode_img.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#-----------------------------
+#filename: umount_whitenode_img.sh
+#卸载 mount_whitenode_img.sh 安装的镜像文件
+#请在使用 mount_whitenode_img.sh 后使用
+#为了安全起见,不删除 mount_whitenode_img.sh建立的安装目录 ${mnt}/{fat,ext4}
+#----------------------------------------------------
+# usage1:
+# umount_whitenode_img.sh mnt_path
+# para:
+# mnt_path:   $1
+# 程序需要root权限
+#-----------------------------------
+# usage2:
+# wget 
+# chmod +x umount_whitenode_img.sh
+# umount_whitenode_img.sh mnt_path
+#-----------------------------------
+
+mnt=$1
+
+echo 注释:如果卸载出错,请退出占用镜像文件的程序
+echo
+
+echo "1.显示镜像安装状况"
+sudo losetup -a
+sudo ls /dev/mapper
+
+echo "2. now start umount ..."
+sudo umount ${mnt}/fat ${mnt}/ext4
+echo "umount ok..."
+
+echo "3. now start mount_whitenode_img.sh"
+sudo  kpartx -d /dev/loop5
+echo "kpartx delete ok ..."
+
+echo "4. now start losetup -d /dev/loop5 ..."
+sudo losetup -d /dev/loop5
+echo "losetup delete ok ..."
+
+#sudo losetup -a
+echo ...All done!
