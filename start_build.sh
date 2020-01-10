e236ecc48fdd60fc24e6002034e56cb97bab15c4
AaG7xNnrgbzeyqc5woPS <47978362+AaG7xNnrgbzeyqc5woPS@users.noreply.github.com>
2020-01-10 21:27:28 +0800

    Update start_build.sh

--- start_build.sh
+++ start_build.sh
@@ -13,6 +13,12 @@ cd ~
 git clone https://github.com/Oizopower/CustomPiOS.git
 git clone https://github.com/AaG7xNnrgbzeyqc5woPS/WhiteNode.git
 
+#patching
+#手工替换
+#替换: https://raw.githubusercontent.com/Oizopower/CustomPiOS/CustomPiOS/src/common.sh
+#用:  https://raw.githubusercontent.com/AaG7xNnrgbzeyqc5woPS/CustomPiOS/CustomPiOS/src/common.sh
+#
+
 cd WhiteNode/src/image
 wget -c --trust-server-names 'https://downloads.raspberrypi.org/raspbian_lite_latest'
 cd ..
